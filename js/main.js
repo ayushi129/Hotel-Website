8453a3234009bbb8cf0656b426f0f80a5af2a096
Ayushi Gupta <ayushigupta0704@gmail.com>
2024-04-14 08:05:59 +0530

    Responsive changes made

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+//HEADER SCROLL
+let nav = document.querySelector('.navbar');
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+};
+
+
+// NAV HIDE
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove('show');
+    })
+})
+
+
 //SWIPER SLIDER
 var swiper = new Swiper('.mySwiper', {
     direction: 'vertical',
