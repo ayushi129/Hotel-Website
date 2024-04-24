83dcee5bd58fa7552edbcd7c861daac3321d1db1
Ayushi Gupta <ayushigupta0704@gmail.com>
2024-04-24 18:12:34 +0530

    Our Partner Section Added to Blog Section

--- js/main.js
+++ js/main.js
@@ -58,3 +58,30 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 console.log(window.screen.width, window.screen.height);
 
+
+// OUR PARTNER
+var swiper = new Swiper(".our-partner", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        '991': {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        },
+        '767': {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        '320': {
+            slidesPerView: 2,
+            spaceBetween: 8,
+        },
+
+
+    },
+});
+
